44197e079c5deff0e5ed7dfcb987c9a8015d71ba
hugogiesteira <hugommg18@gmail.com>
2015-12-11 15:24:20 +0000

    [update] faster searchableCombo

--- src/main/java/pt/uminho/ceb/biosystems/mew/guiutilities/gui/components/searchableCombo/SearchableComboBox.java
+++ src/main/java/pt/uminho/ceb/biosystems/mew/guiutilities/gui/components/searchableCombo/SearchableComboBox.java
@@ -3,17 +3,26 @@ package pt.uminho.ceb.biosystems.mew.guiutilities.gui.components.searchableCombo
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseListener;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -26,7 +35,6 @@ import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
-import javax.swing.UIManager;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
@@ -58,9 +66,11 @@ public class SearchableComboBox extends JComboBox {
 	private boolean lastStatus = true;
 	
 	private boolean typing = false;
-	private MouseListener[] comboButtonMouseListeners;
+	private MouseListener[] originalComboButtonMouseListeners, comboButtonMouseListeners;
 	
+	private boolean listenersAlreadyInitialized = false;
 	
+//	protected Color defaultColor;
 	public TreeSet<String> getValues(){
 		  return allItemsList;
 	}
@@ -79,6 +89,11 @@ public class SearchableComboBox extends JComboBox {
 		strAuto = initial;
 		
 		listeners = new HashSet<IValidatedComboSearch>();
+		
+		for (Component comp : getComponents())
+			if(comp instanceof JButton)
+				originalComboButtonMouseListeners = comp.getMouseListeners();
+		
 		initialize();
 	}
 	
@@ -126,11 +141,20 @@ public class SearchableComboBox extends JComboBox {
 			if(comp instanceof JButton)
 				comboButtonMouseListeners = comp.getMouseListeners();
 		
+		triggerFilledCombo();
+		
+		if(!listenersAlreadyInitialized)
+			addListeners();
+	}
+	
+	protected void addListeners(){
 		addKeyListenerToComboBox();
 		
 		addFocusListenerToComboBox();
 		
 		addDocumentListenerToComboBoxEditor();
+		
+		listenersAlreadyInitialized = true;
 	}
 	
 	// Variable used to prevent multiple focusEvents 
@@ -140,10 +164,8 @@ public class SearchableComboBox extends JComboBox {
 
 			@Override
 			public void focusLost(FocusEvent e) {
-				if(!lostFocus)
-				{
-					if(getItemCount() > 0 && !allItemsArrayList.contains(strAuto))
-					{
+				if(!lostFocus) {
+					if(getItemCount() > 0 && !allItemsArrayList.contains(strAuto)) {
 						setSelectedIndex(0);
 						repopulatePopUpList(getItemAt(0));
 					}
@@ -168,8 +190,7 @@ public class SearchableComboBox extends JComboBox {
 			
 			@Override
 			public void insertUpdate(DocumentEvent e) {
-				if(!typing)
-				{
+				if(!typing) {
 					strAuto = getEditor().getItem().toString();
 					verifyValueCombo();
 				}
@@ -183,7 +204,6 @@ public class SearchableComboBox extends JComboBox {
 	
 
 	private void addKeyListenerToComboBox() {
-		final Color colorDefault = ((JTextField)getEditor().getEditorComponent()).getForeground();
 		getEditor().getEditorComponent().addKeyListener(new KeyListener() {
 			
 			@Override
@@ -193,9 +213,10 @@ public class SearchableComboBox extends JComboBox {
 			@Override
 			public void keyReleased(KeyEvent e) {
 				typing = true;
+				
 				if(!codes.contains(e.getKeyCode()) ){
-					setPopupVisible(false);
-			    	  
+					hidePopup();
+					
 					strAuto = ((JTextField)getEditor().getEditorComponent()).getText();
 			    	int pos =((JTextField)getEditor().getEditorComponent()).getCaretPosition();
 	
@@ -204,32 +225,10 @@ public class SearchableComboBox extends JComboBox {
 			    	removeAllItems();
 			    	  
 			    	if(objL.size() == 0)
-			    	{
-			    		((JTextField)getEditor().getEditorComponent()).setForeground(Color.RED);
-			    		for (Component comp : getComponents()) {
-							if(comp instanceof AbstractButton)
-							{
-								comp.setEnabled(false);
-								//Remove listeners
-								for (MouseListener mouseListener : comp.getMouseListeners())
-									comp.removeMouseListener(mouseListener);
-							}
-						}
-			    	}
-			    	else
-			    	{
-			    		for (Component comp : getComponents()) {
-							if(comp instanceof AbstractButton)
-							{
-								if(!comp.isEnabled()){
-									comp.setEnabled(true);
-									//Add original listeners
-									for (MouseListener mouseListener : comboButtonMouseListeners)
-										comp.addMouseListener(mouseListener);
-								}
-							}
-						}
-			    		((JTextField)getEditor().getEditorComponent()).setForeground(colorDefault);
+			    		triggerEmptyCombo();
+			    	
+			    	else {
+			    		triggerFilledCombo();
 			    		
 			    		for (Object item : objL)
 			    			addItem(item);
@@ -239,16 +238,14 @@ public class SearchableComboBox extends JComboBox {
 			    	}
 			    	  
 			    	if(objL.size() != 0)
-			    		setPopupVisible(true);
+			    		showPopup();
 			    	
 			    	((JTextField)getEditor().getEditorComponent()).setText(strAuto);
 			    	((JTextField)getEditor().getEditorComponent()).setCaretPosition(pos);
 				}
 								
-				if((e.getKeyCode() == 10 || e.getKeyCode() == 27) && getItemCount() > 0) //Enter and Esc
-				{
-					if(getItemAt(getSelectedIndex()) == null)
-					{
+				if((e.getKeyCode() == 10 || e.getKeyCode() == 27) && getItemCount() > 0) { //Enter and Esc
+					if(getItemAt(getSelectedIndex()) == null) {
 						((JTextField)getEditor().getEditorComponent()).setText(getItemAt(0).toString());
 						setSelectedIndex(0);
 					}
@@ -260,7 +257,6 @@ public class SearchableComboBox extends JComboBox {
 				verifyValueCombo();
 				
 				typing = false;
-				
 			}
 			
 			@Override
@@ -268,7 +264,32 @@ public class SearchableComboBox extends JComboBox {
 			}
 		});
 	}
+	
+	protected void triggerEmptyCombo(){
+		((JTextField)getEditor().getEditorComponent()).setForeground(Color.RED);
+		for (Component comp : getComponents()) {
+			if(comp instanceof AbstractButton) {
+				comp.setEnabled(false);
+				//Remove listeners
+				for (MouseListener mouseListener : comp.getMouseListeners())
+					comp.removeMouseListener(mouseListener);
+			}
+		}
+	}
 
+	protected void triggerFilledCombo(){
+		((JTextField)getEditor().getEditorComponent()).setForeground(Color.BLACK);
+		for (Component comp : getComponents()) {
+			if(comp instanceof AbstractButton) {
+				if(!comp.isEnabled()){
+					comp.setEnabled(true);
+					//Add original listeners
+					for (MouseListener mouseListener : originalComboButtonMouseListeners)
+						comp.addMouseListener(mouseListener);
+				}
+			}
+		}
+	}
 	
 	private void repopulatePopUpList(Object selectedItem) {
 		setModel(new DefaultComboBoxModel(allItemsList.toArray()));
@@ -280,7 +301,10 @@ public class SearchableComboBox extends JComboBox {
 		this.allItemsList = list;
 		strAuto = initial;
 		setModel(new DefaultComboBoxModel(list.toArray()));
-		this.setSelectedIndex(getIndexOfInitial(initial, list));
+		int index = getIndexOfInitial(initial, list);
+		if(index >= 0 && list.size() != 0)
+			this.setSelectedIndex(index);
+		
 		initialize();
 	}
 	
@@ -290,15 +314,17 @@ public class SearchableComboBox extends JComboBox {
 	}
 	
 	public void removeValue(String text){
-		allItemsList.remove(text);
-		setNewValues(allItemsList, "");
+		if(text != null && !text.equals("") && allItemsList.contains(text)){
+			allItemsList.remove(text);
+			setNewValues(allItemsList, "");
+		}
 	}
 
 	private int getIndexOfInitial(String initial, TreeSet<String> list) {
-		int index= 0;
+		int index = 0;
 		int counter = 0;
 		
-		for(String s : list){
+		for(String s : list) {
 			if(s.equals(initial))
 				index = counter;
 			counter++;
@@ -318,7 +344,7 @@ public class SearchableComboBox extends JComboBox {
 	@Override
 	public void updateUI(){
 		super.updateUI();
-		initialize();
+//		initialize();
 	}
 	
 	public boolean isComboBoxValid(){
@@ -415,60 +441,149 @@ public class SearchableComboBox extends JComboBox {
 		frame.dispose();
 	}
 	
+//	public static void main(String[] args) {
+//		SearchableComboBox.createPanel(null, null);
+//		JFrame frame = new JFrame();
+//		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+//		frame.setLayout(new GridLayout(1, 2));
+//		final SearchableComboBox combo = new SearchableComboBox();
+//		
+//		combo.addValidatedComboSearchListener(new IValidatedComboSearch() {
+//			
+//			@Override
+//			public void changedStatus(String s) {
+//				System.out.println("PVVVVVVVVVVVVV " + s);
+//				
+//			}
+//		});
+//		
+//		TreeSet<String> list = new TreeSet<String>();
+//		String initial = "REACTION";
+//		list.add("TEST");
+//		list.add("COMBO");
+//		list.add("REACTION");
+//		list.add("GENE");
+//		list.add("R_EX_X");
+//		ArrayList<String[]> items = new ArrayList<String[]>();
+//		String[] strL = {"R_EX_glc_e","R_ICDHyr","R_Biomass_Ecoli_core_w_GAM","R_EX_nh4_e","R_PGK","R_PGM","R_EX_pi_e","R_GAPD","R_ACONTa","R_ACONTb","R_GLCpts","R_GLNS","R_EX_h_e","R_PIt2r","R_ENO","R_CS","R_NH4t","R_RPI", "A_dxs"};
+//		items.add(strL);
+//		
+//		for (String string : strL) {
+//			list.add(string);
+//		}
+//
+//		combo.setNewValues(list, initial);
+//		System.out.println("Number: "+combo.getSelectedIndex()+"");
+//		System.out.println(combo.isComboBoxValid());
+//
+//		//textfield = new JTextField();
+//		frame.add(combo); 
+//		frame.add(new JTextField(""));
+//		frame.add(new JButton("B"));
+//
+//		Icon icon;
+//		//Image img = ((ImageIcon) UIManager.getIcon("OptionPane.informationIcon")).getImage() ;
+//		String path = "/home/hgiesteira/Desktop/EclipseProjects/OptFlux/optfluxcore3/plugins_src/optflux.simulation/images/icons/s16x16/icon-sbml.png";
+//		Image img = new ImageIcon(path).getImage();
+//		Image newimg = img.getScaledInstance( 48, 48,  java.awt.Image.SCALE_SMOOTH ) ;  
+//		icon = new ImageIcon( newimg );
+//		JLabel warnErrorLabel = new JLabel("TEST", UIManager.getIcon("OptionPane.warningIcon")/*new ImageIcon(ERRORICONPATH)*/, JLabel.LEFT);
+//		JLabel warnErrorLabel2 = new JLabel("TEST", icon, JLabel.LEFT);
+//		frame.add(warnErrorLabel);
+//		frame.add(warnErrorLabel2);
+//		
+//		//frame.add(textfield);
+//		frame.setVisible(true);
+//		frame.pack();
+//	}
+	
 	public static void main(String[] args) {
-		SearchableComboBox.createPanel(null, null);
-		JFrame frame = new JFrame();
-		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-		frame.setLayout(new GridLayout(1, 2));
+		JFrame frame = new JFrame("Test");
+		
+		frame.setLayout(new FlowLayout());
+		
+		
+		Scanner s = null;
+		
+		final TreeSet<String> list = new TreeSet<String>();
+
+        try {
+            s = new Scanner(new BufferedReader(new FileReader("/home/hgiesteira/Desktop/ToDelete/Names")));
+
+            while (s.hasNext()) {
+            	list.add(s.next());
+            }
+        } catch (FileNotFoundException e) {
+        	if (s != null) {
+                s.close();
+            }
+			e.printStackTrace();
+		} finally {
+			if (s != null) {
+                s.close();
+            }
+        }
+		
 		final SearchableComboBox combo = new SearchableComboBox();
 		
-		combo.addValidatedComboSearchListener(new IValidatedComboSearch() {
+		
+		JComboBox cmb = new JComboBox();
+		cmb.setEditable(true);
+		cmb.getEditor().getEditorComponent().addKeyListener(new KeyAdapter() {
+
+		    @Override
+		    public void keyReleased(KeyEvent event) {
+		    	System.out.println("please dont make me blank");
+//		        if (event.getKeyChar() == KeyEvent.VK_ENTER) {
+//		            if (((JTextComponent) ((JComboBox) ((Component) event
+//		                    .getSource()).getParent()).getEditor()
+//		                    .getEditorComponent()).getText().isEmpty())
+//		                System.out.println("please dont make me blank");
+//		        }
+		    }
+		});
+		
+		JButton button = new JButton("Change");
+		
+		button.addActionListener(new ActionListener() {
 			
 			@Override
-			public void changedStatus(String s) {
-				System.out.println("PVVVVVVVVVVVVV " + s);
-				
+			public void actionPerformed(ActionEvent e) {
+				combo.setNewValues(list, "Mills");
 			}
 		});
 		
-		TreeSet<String> list = new TreeSet<String>();
-		String initial = "REACTION";
-		list.add("TEST");
-		list.add("COMBO");
-		list.add("REACTION");
-		list.add("GENE");
-		list.add("R_EX_X");
-		ArrayList<String[]> items = new ArrayList<String[]>();
-		String[] strL = {"R_EX_glc_e","R_ICDHyr","R_Biomass_Ecoli_core_w_GAM","R_EX_nh4_e","R_PGK","R_PGM","R_EX_pi_e","R_GAPD","R_ACONTa","R_ACONTb","R_GLCpts","R_GLNS","R_EX_h_e","R_PIt2r","R_ENO","R_CS","R_NH4t","R_RPI", "A_dxs"};
-		items.add(strL);
+		JComboBox<String> normalCombo = new JComboBox<>();
+		List<String> l = new ArrayList<>();
+		l.add("Ab");
 		
-		for (String string : strL) {
-			list.add(string);
+		for (String string : list) {
+			normalCombo.addItem(string);
 		}
-
-		combo.setNewValues(list, initial);
-		System.out.println("Number: "+combo.getSelectedIndex()+"");
-		System.out.println(combo.isComboBoxValid());
-
-		//textfield = new JTextField();
-		frame.add(combo); 
-		frame.add(new JTextField(""));
-		frame.add(new JButton("B"));
-
-		Icon icon;
-		//Image img = ((ImageIcon) UIManager.getIcon("OptionPane.informationIcon")).getImage() ;
-		String path = "/home/hgiesteira/Desktop/EclipseProjects/OptFlux/optfluxcore3/plugins_src/optflux.simulation/images/icons/s16x16/icon-sbml.png";
-		Image img = new ImageIcon(path).getImage();
-		Image newimg = img.getScaledInstance( 48, 48,  java.awt.Image.SCALE_SMOOTH ) ;  
-		icon = new ImageIcon( newimg );
-		JLabel warnErrorLabel = new JLabel("TEST", UIManager.getIcon("OptionPane.warningIcon")/*new ImageIcon(ERRORICONPATH)*/, JLabel.LEFT);
-		JLabel warnErrorLabel2 = new JLabel("TEST", icon, JLabel.LEFT);
-		frame.add(warnErrorLabel);
-		frame.add(warnErrorLabel2);
 		
-		//frame.add(textfield);
+		combo.setNewValues(list, "Mills");
+		
+		frame.add(combo);
+		frame.add(normalCombo);
+		frame.add(button);
+		
+		
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
 		frame.setVisible(true);
 		frame.pack();
+		
+//		System.out.println("AGORA!");
+//		long init = System.currentTimeMillis();
+//		normalCombo.setPopupVisible(false);
+//		
+//		System.out.println("T: " + (System.currentTimeMillis() - init));
+//		long init2 = System.currentTimeMillis();
+//		
+//		System.out.println("AGAIN");
+//		
+//		normalCombo.setPopupVisible(true);
+//		System.out.println("T: " + (System.currentTimeMillis() - init2));
 	}
 	//static JTextField textfield;
 	
